14d7fc9dc94f99b0e91c73e92f76a3699e251a42
Maksymilian Pietras <115542149+czisCierpiacy@users.noreply.github.com>
2023-10-21 11:14:28 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Online gradebook</title>
+    <meta name="description" content="Welcome to my gradebook">
+    <link rel="stylesheet" href="styles/loginStyles.css">
+</head>
+<body>
+    <div class="main_container">
+        
+
+        <div id="login_field">
+            <form id="login_form">
+                <label for="email" class="login_label">Wpisz email:</label>
+                <input type="email" name="email" class="login_input" placeholder="podaj email...">
+
+                <label for="password" class="login_label">Wpisz hasło:</label>
+                <input type="password" name="password" class="login_input" placeholder="podaj hasło...">
+
+                <div class="button_container">
+                    <input type="submit" value="Zaloguj">
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+
+</body>
+</html>
