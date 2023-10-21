8e87fe9684a3f646ec90a956490a16ccebe04fd6
Maksymilian Pietras <240774@edu.p.lodz.pl>
2023-10-21 14:16:25 +0200

    Login and register card animation after hover

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <title>Online gradebook</title>
     <meta name="description" content="Welcome to my gradebook">
     <link rel="stylesheet" href="styles/loginStyles.css">
+    <link rel="stylesheet" href="styles/loginRegisterCardAnim.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,25 +14,40 @@
 <body>
     <div class="main_container">
 
-
-        <div id="login_field">
-            <div id="online_gradebook_logo">
-                <img src="images/gradebook_logo.png">
-            </div>
-
-            <form id="login_form">
-                <label for="email" class="login_label">Wpisz email:</label>
-                <input type="email" name="email" class="login_input" placeholder="podaj email...">
-
-                <label for="password" class="login_label">Wpisz hasło:</label>
-                <input type="password" name="password" class="login_input" placeholder="podaj hasło...">
-
-                <div class="button_container">
-                    <input type="submit" id="login_button" value="Zaloguj">
+        <div id="main_pane">
+            <div id="login_register_card">
+                <div id="login_card" class="card_button_container">
+                    <button type="button" class="card_button">Logowanie</button>
                 </div>
 
-            </form>
+                <div id="register_card" class="card_button_container">
+                    <button type="button" class="card_button">Rejestracja</button>
+                </div>
+            </div>
+
+
+            <div id="login_field">
+
+
+                <div id="online_gradebook_logo">
+                    <img src="images/gradebook_logo.png">
+                </div>
+
+                <form id="login_form">
+                    <label for="email" class="login_label">Wpisz email:</label>
+                    <input type="email" name="email" class="login_input" placeholder="podaj email...">
+
+                    <label for="password" class="login_label">Wpisz hasło:</label>
+                    <input type="password" name="password" class="login_input" placeholder="podaj hasło...">
+
+                    <div class="button_container">
+                        <input type="submit" id="login_button" value="Zaloguj">
+                    </div>
+                </form>
+
+            </div>
         </div>
+
     </div>
 
 
