65b27a51a144248481efd95d3aab88d50b613aad
Maksymilian Pietras <240774@edu.p.lodz.pl>
2023-10-22 19:39:00 +0200

    Create account info

--- scripts/menuAjaxScripts.js
+++ scripts/menuAjaxScripts.js
@@ -6,11 +6,14 @@ $(document).ready(function (){
             url: "./serverActions/registerActions.php",
             data: $(this).serialize(),
             success:function (response) {
-
+                $("#register_form_container").text("Pomyślnie utworzono konto!\nteraz możesz sie zalogować")
+                    .css("color", "green").css("font-size", "30px")
+                    .css("text-shadow", "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000");
 
             },
             error: function (response){
-                $("#incorrect_teacher_password").text("Niepoprawne hasło nauczyciela").css("color", "red");
+                $("#incorrect_teacher_password").text("Niepoprawne hasło nauczyciela").css("color", "red")
+                    .css("text-shadow", "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000");
 
             }
         })
