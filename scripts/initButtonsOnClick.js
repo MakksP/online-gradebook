aed3c9d95c7222f72bcc65d6ce8121fa4372c87c
Maksymilian Pietras <240774@edu.p.lodz.pl>
2023-10-28 16:33:55 +0200

    Bigger icons

--- scripts/initButtonsOnClick.js
+++ scripts/initButtonsOnClick.js
@@ -2,8 +2,6 @@ const subjects_buttons = document.getElementsByClassName("subject_button");
 const not_teaching_subjects_buttons = document.getElementsByClassName("not_teaching_subject_button");
 const not_teaching_subjects_labels = document.getElementsByClassName("not_teaching_subject_label");
 const add_subject_button = document.getElementById("new_subject_button");
-const register_card = document.getElementById("register_card");
-const login_card = document.getElementById("login_card");
 const ADD_SUBJECT_BUTTON = 1;
 
 
@@ -30,24 +28,6 @@ for (let button_index = 0; button_index < subjects_buttons.length - ADD_SUBJECT_
 
 }
 
-if (login_card != null){
-    login_card.onclick = function (){
-        changeCardsColor("login_card", "register_card");
-        document.getElementById("login_field").style.height = "40%";
-        document.getElementById("register_form").innerHTML = ""
-        fill_login_form();
-    }
-}
-
-
-if (register_card != null){
-    register_card.onclick = function (){
-        changeCardsColor("register_card", "login_card")
-        document.getElementById("login_field").style.height = "80%";
-        document.getElementById("login_form").innerHTML = ""
-        fill_register_form();
-    }
-}
 
 if (add_subject_button != null){
     add_subject_button.onclick = function (){
