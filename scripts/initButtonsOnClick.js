f8fd51581878c856dc0ee2f1f8d32b1874b4bf63
Maksymilian Pietras <240774@edu.p.lodz.pl>
2023-11-02 19:25:06 +0100

    Grid -> borders (bottom and right)

--- scripts/initButtonsOnClick.js
+++ scripts/initButtonsOnClick.js
@@ -28,6 +28,7 @@ function add_header_to_grades_table() {
 }
 
 
+
 for (let button_index = 0; button_index < subjects_buttons.length - ADD_SUBJECT_BUTTON; button_index+=1){
     if (subjects_buttons[button_index] != null){
         subjects_buttons[button_index].onclick = function () {
