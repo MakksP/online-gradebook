d883c26149a2b17927206ed8700f1bc51567d6d7
Maksymilian <maksim2@op.pl>
2023-11-20 12:35:59 +0100

    Calculating average in student archive grades

--- serverActions/studentDataActions/getAndDrawStudentData.php
+++ serverActions/studentDataActions/getAndDrawStudentData.php
@@ -9,25 +9,33 @@ function get_and_draw_student_data(){
 
     if ($prepared_sql_query = $db_connection->prepare($sql_query)){
         $prepared_sql_query->bind_param("s", $_GET["student_email"]);
+        $tmp_subject_name = "";
         if ($prepared_sql_query->execute()){
             $prepared_sql_query->bind_result($subjectName, $grade);;
-            $tmp_subject_name = "";
+            declare_variables();
             while ($prepared_sql_query->fetch()){
                 $main_div_name = $subjectName . "_subject_div";
                 $subject_name_div = $subjectName . "_subject_name_div";
                 $grades_name_div = $subjectName . "_grades_name_div";
                 $average_name_div = $subjectName . "_average_name_div";
+
                 if ($tmp_subject_name != $subjectName){
+                    if ($tmp_subject_name != ""){
+                        $tmp_average_name_div = $tmp_subject_name . "_average_name_div";
+                        $tmp_grades_name_div = $tmp_subject_name . "_grades_name_div";
+                        calculate_student_average_from_subject($tmp_grades_name_div, $tmp_average_name_div);
+                    }
                     create_new_container_for_subject($main_div_name, $subject_name_div,
                         $subjectName, $grades_name_div, $grade, $average_name_div);
                     $tmp_subject_name = $subjectName;
                 } else{
-                    echo "<script> 
-                            document.getElementById('$grades_name_div').insertAdjacentHTML('beforeend', ', $grade');
-                        </script>";
+                    add_next_grade_to_table($grades_name_div, $grade);
                 }
 
             }
+            $tmp_average_name_div = $tmp_subject_name . "_average_name_div";
+            $tmp_grades_name_div = $tmp_subject_name . "_grades_name_div";
+            calculate_student_average_from_subject($tmp_grades_name_div, $tmp_average_name_div);
 
         } else{
             $prepared_sql_query->close();
@@ -44,6 +52,47 @@ function get_and_draw_student_data(){
     $prepared_sql_query->close();
     $db_connection->close();
 }
+
+/**
+ * @return void
+ */
+function declare_variables(): void
+{
+    echo "<script>let grades;</script>";
+}
+
+/**
+ * @param string $tmp_grades_name_div
+ * @param string $tmp_average_name_div
+ * @return void
+ */
+function calculate_student_average_from_subject(string $tmp_grades_name_div, string $tmp_average_name_div): void
+{
+    echo "<script>
+                                grades = document.getElementById('$tmp_grades_name_div').innerHTML;
+                                grades = grades.match(/\d+/g)
+                                grades = grades.map(grade => {
+                                    return parseInt(grade, 10);
+                                });
+                                
+                                avg = grades.reduce((sum, next_grade) => sum + next_grade);
+                                avg = avg / grades.length;
+                                document.getElementById('$tmp_average_name_div')
+                              .insertAdjacentHTML('beforeend', avg.toFixed(2)) </script>";
+}
+
+/**
+ * @param string $grades_name_div
+ * @param $grade
+ * @return void
+ */
+function add_next_grade_to_table(string $grades_name_div, $grade): void
+{
+    echo "<script> 
+                            document.getElementById('$grades_name_div').insertAdjacentHTML('beforeend', ', $grade');
+                        </script>";
+}
+
 
 /**
  * @param string $main_div_name
