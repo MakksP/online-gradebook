3d43b6b8f450812f5714abafe234cb29fead80b1
Maksymilian <maksim2@op.pl>
2023-11-24 11:32:15 +0100

    Fixed not getting favourite subject details in teacher

--- serverActions/favouriteActions/getAndDrawFavourite.php
+++ serverActions/favouriteActions/getAndDrawFavourite.php
@@ -11,12 +11,19 @@ function get_and_draw_favourites(){
         $prepared_sql_query->bind_param("s", $_SESSION["userId"]);
         if ($prepared_sql_query->execute()){
             $prepared_sql_query->bind_result($subject);
-
             while ($prepared_sql_query->fetch()){
-                echo "<div class='subject_div'>
+                if ($_SESSION["role"] == "student"){
+                    echo "<div class='subject_div'>
                    <button type='button' class='subject_button_s'>$subject</button>
                    <button class='delete_favourite_button' style='grid-column: 1/3'><i class='icon-block'></i></button>
                </div>";
+                } else if ($_SESSION["role"] == "teacher"){
+                    echo "<div class='subject_div'>
+                   <button type='button' class='subject_button'>$subject</button>
+                   <button class='delete_favourite_button' style='grid-column: 1/3'><i class='icon-block'></i></button>
+               </div>";
+                }
+
             }
 
         } else{
