857d67560fa96fb66e9f3687687614a413c70a25
Maksymilian <maksim2@op.pl>
2023-11-26 14:24:33 +0100

    Email, name, surname regex checking while login/registration

--- scripts/menuAjaxScripts.js
+++ scripts/menuAjaxScripts.js
@@ -1,6 +1,30 @@
+const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$/;
+const name_surname_pattern = /^[A-Z][a-z]+$/
+
 $(document).ready(function (){
     $("#registration_form").on('submit', function (e){
         e.preventDefault();
+
+        const user_email = $(this).find('input[name="email"]').val();
+        if (email_pattern.test(user_email) === false){
+            document.getElementById("incorrect_email_password").innerText
+                = "Email nie spełnia wzorca text@text.text"
+            return;
+        }
+        const user_name = $(this).find('input[name="name"]').val();
+        if (name_surname_pattern.test(user_name) === false){
+            document.getElementById("incorrect_email_password").innerText
+                = "Imię nie zaczyna się z wielkiej litery\nlub zawiera niedozwolone znaki"
+            return;
+        }
+
+        const user_surname = $(this).find('input[name="surname"]').val();
+        if (name_surname_pattern.test(user_surname) === false){
+            document.getElementById("incorrect_email_password").innerText
+                = "Nazwisko nie zaczyna się z wielkiej litery\nlub zawiera niedozwolone znaki"
+            return;
+        }
+
         $.ajax({
             type: "POST",
             url: "./serverActions/registerActions.php",
@@ -24,6 +48,12 @@ $(document).ready(function (){
 $(document).ready(function (){
     $("#login_form").on('submit', function (e){
         e.preventDefault();
+        const user_email = $(this).find('input[name="email"]').val();
+        if (email_pattern.test(user_email) === false){
+            document.getElementById("incorrect_email_password").innerText
+                = "Email nie spełnia wzorca text@text.text"
+            return;
+        }
         $.ajax({
             type: "POST",
             url: "./serverActions/loginActions.php",
@@ -33,8 +63,7 @@ $(document).ready(function (){
                 window.location.href = response["redirect"];
             },
             error: function (response){
-                $("#incorrect_email_password").text("Nieprawidłowy email lub hasło").css("color", "red")
-                    .css("text-shadow", "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000");
+                $("#incorrect_email_password").text("Nieprawidłowy email lub hasło")
             }
         })
     })
